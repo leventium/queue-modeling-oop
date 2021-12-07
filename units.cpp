64ed2607c481ed3348ad8a6f23ddd37d27d67ea1
Leon <limiroshnichenko@miem.hse.ru>
2021-12-07 19:26:28 +0300

    I've did polymorphizm

--- units.cpp
+++ units.cpp
@@ -16,3 +16,31 @@ int OfferUnit::getType()
 {
     return this->type;
 }
+void OfferUnit::setUnique(int info)
+{
+    this->stInfo = info;
+}
+int OfferUnit::getUnique()
+{
+    return this->stInfo;
+}
+
+
+int QueueUnit::getUnique()
+{
+    return this->time;
+}
+void QueueUnit::setUnique(int time)
+{
+    this->time = time;
+}
+
+
+int StatUnit::getUnique()
+{
+    return this->status;
+}
+void StatUnit::setUnique(int status)
+{
+    this->status = status;
+}
--- units.h
+++ units.h
@@ -1,11 +1,31 @@
-
 class OfferUnit
 {
 private:
     int number, type;
+    int stInfo;
 public:
     void setNumber(int number);
     int getNumber();
     void setType(int type);
     int getType();
+    virtual void setUnique(int info);
+    virtual int getUnique();
+};
+
+class QueueUnit : public OfferUnit
+{
+private:
+    int time;
+public:
+    virtual void setUnique(int time);
+    virtual int getUnique();
+};
+
+class StatUnit : public OfferUnit
+{
+private:
+    int status;
+public:
+    virtual void setUnique(int status);
+    virtual int getUnique();
 };
