fd294a84a63d6ba7e41e028974d2c46bca3ca8d6
Leon <limiroshnichenko@miem.hse.ru>
2021-12-07 23:37:58 +0300

    First description

---
+++ collection.h
@@ -0,0 +1,20 @@
+template <typename T>
+class UnitsCollection
+{
+private:
+    struct node
+    {
+        T info;
+        node *prev;
+        node *next;
+    };
+    node *first;
+public:
+    UnitsCollection();
+    ~UnitsCollection();
+    int size();
+    const T& operator[](const int &i);
+    void operator<<(const T &right);
+    void insert(const int &i, const T &right);
+    void del(const int &i);
+};
