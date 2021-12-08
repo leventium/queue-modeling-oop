989ee596bc052a01e81a75c72e5e608ffe6840c7
Фролов Кирилл <kdfrolov@miem.hse.ru>
2021-12-08 23:59:38 +0300

    change comments

--- kirill_control.cpp
+++ kirill_control.cpp
@@ -6,7 +6,7 @@ int Control::read(void)
 	int n, k, f = 0;
 	do
 	{
-		cout << " (введите целое положительное число) " << endl;
+		cout << " (enter a positive integer) " << endl;
 		cin >> str;
 		k = cin.fail();
 		if (!k)
@@ -41,7 +41,7 @@ void Control::enter(int& kass, UnitsCollection<QueueUnit>& queue, UnitsCollectio
 {
 	int i;
 	{
-		cout << "¬ведите количество типов за€вок(максимально 5)";
+		cout << "Enter the number of application types (maximum 5)";
 		nType = read();
 	} while ((nType > 5));
 
@@ -49,26 +49,26 @@ void Control::enter(int& kass, UnitsCollection<QueueUnit>& queue, UnitsCollectio
 	{
 		do
 		{
-			cout << "¬ведите дл€ " << (char)(65 + i) << " типа врем€ обработки за€вки (в секундах от 1 до 30)";
+			cout << "Enter for " << (char)(65 + i) << " type application processing time (in seconds from 1 to 30)";
 			times[i] = read();
 		} while ((times[i] > 30));
 	}
 
 	do
 	{
-		cout << "¬ведите длительность рабочего дн€ в секундах(в секундах от 30 до 300)";
+		cout << "Enter the length of the working day in seconds (in seconds from 30 to 300)";
 		dayTime = read();
 	} while ((dayTime < 30) || (dayTime > 300));
 
 	do
 	{
-		cout << "¬ведите количество касс(максимально 5)";
+		cout << "Enter the number of cash registers (maximum 5)";
 		kass = read();
 	} while ((kass > 5));
 
 	do
 	{
 		check(queue, stat, nstat, nqueue, n, *timework, *kass, time, *ntype);
-	} while (queue.size == 0); //случай, когда пользователь ничего не ввел или ввел не те символы
+	} while (queue.size == 0); //the case when the user did not enter anything or entered the wrong characters
 
 }
