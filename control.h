1bec1d6dd51e0fe704b2f410634965a519bd79b8
Leon <limiroshnichenko@miem.hse.ru>
2021-12-08 20:53:46 +0300

    control init

--- control.h
+++ control.h
@@ -1,6 +1,22 @@
 #ifndef CONTROLL
 #define CONTROLL
 
+class Control
+{
+private:
+    int dayTime, nType, times[5];
+    // dayTime - заданное время дня
+    // nType - заданное количество типов
+    // times[] - заданное время обработки каждого типа
+public:
+    void printQueue();
+    void printStat();
+    int queueCount();
+    void makeStat();
 
+    int read();
+    void check();
+    void enter();
+};
 
 #endif CONTROLL
