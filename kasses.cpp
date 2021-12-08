cccdeb7d10cdddc0ce9316c07be99ae3ce1d8e28
Leon <limiroshnichenko@miem.hse.ru>
2021-12-09 00:35:39 +0300

    first try

--- kasses.cpp
+++ kasses.cpp
@@ -1,1 +1,27 @@
 #include "kasses.h"
+
+int Kasses::getAmount() const { return amount; }
+void Kasses::setAmount(int amount) { this->amount = amount; }
+QueueUnit& Kasses::operator[](const int &i)
+{
+    switch (i)
+    {
+        case 0:
+            return kas0;
+            break;
+        case 1:
+            return kas1;
+            break;
+        case 2:
+            return kas2;
+            break;
+        case 3:
+            return kas3;
+            break;
+        case 4:
+            return kas4;
+            break;
+        default:
+            throw "index out of range";
+    }
+}
--- kasses.h
+++ kasses.h
@@ -1,6 +1,17 @@
 #ifndef KASSES
 #define KASSES
 
+#include "units.h"
 
+class Kasses
+{
+private:
+    int amount;
+    QueueUnit kas0, kas1, kas2, kas3, kas4;
+public:
+    int getAmount() const;
+    void setAmount(int amount);
+    QueueUnit& operator[](const int &i);
+};
 
 #endif KASSES
