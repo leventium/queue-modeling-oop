9682963bfa6f1799825a854e31dd764ef53bce37
Leon <limiroshnichenko@miem.hse.ru>
2021-12-09 15:47:01 +0300

    new main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
-#include "units.h"
-//#include "collection.h"
-#include "control.h"
-//#include "kirill_control.cpp"
-#include "collection.cpp"
-#include "kasses.h"
 
 int main()
 {
-    Kasses kass;
-    QueueUnit test;
-    UnitsCollection<QueueUnit> queue;
-    UnitsCollection<StatUnit> stat;
-    int n = 0;
-    test.setNumber(10);
-    kass.setAmount(3);
-    kass[0] = test;
-    
-    std::cout << kass[2].getNumber() << "\n\n";
 
-    Control control;
-    control.enter(kass, queue, stat, n);
     return 0;
 }
